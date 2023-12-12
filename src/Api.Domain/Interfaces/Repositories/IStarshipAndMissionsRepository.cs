1fea61f036d328b10557f7ade2805541fc068ca6
fumas <vinifumagalli_@hotmail.com>
2023-12-12 14:40:51 -0300

    ei

--- src/Api.Domain/Interfaces/Repositories/IStarshipAndMissionsRepository.cs
+++ src/Api.Domain/Interfaces/Repositories/IStarshipAndMissionsRepository.cs
@@ -1,6 +1,15 @@
+using Api.Domain.Entities;
+
 namespace Api.Domain.Interfaces.Repositories;
 
 public interface IStarshipAndMissionsRepository
 {
+    Task<List<StarshipEntity>> GetStarships();
+    Task<StarshipEntity?> GetStarshipByName(string name);
+    Task<StarshipEntity?> UpdateStarship(string name, StarshipEntity starship);
+    Task<bool> DeleteStarship(string name);
+    Task<StarshipEntity?> GetStarshipByModel(string model);
+    Task<StarshipEntity?> GetStarshipByManufacturer(string manufacturer);
+    Task<StarshipEntity?> CreateStarship(StarshipEntity starship);
 
 }
