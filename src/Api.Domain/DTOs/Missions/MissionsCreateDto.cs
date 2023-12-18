1190dac76433951171ccdc59663b6f29ea2cfde0
Fumas <vinifumagalli_@hotmail.com>
2023-12-17 23:26:48 -0300

    refactor(MissionsCreateDto): retira atributo da DTO para ser coerente com nova lógica implementada

--- src/Api.Domain/DTOs/Missions/MissionsCreateDto.cs
+++ src/Api.Domain/DTOs/Missions/MissionsCreateDto.cs
@@ -2,7 +2,6 @@ namespace Api.Domain.DTOs.Missions;
 
 public class MissionsCreateDto
 {
-    public string? StarshipName { get; set; }
     public string? Goal { get; set; }
     public string? Date { get; set; }
     public string? Duration { get; set; }
