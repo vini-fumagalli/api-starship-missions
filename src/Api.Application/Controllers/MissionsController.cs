ec18935cd95e6193634f2a6a06740ea3a8ebcb7c
Fumas <vinifumagalli_@hotmail.com>
2023-12-18 20:14:04 -0300

    refactor (Controllers): adiciona comentários explicativos

--- src/Api.Application/Controllers/MissionsController.cs
+++ src/Api.Application/Controllers/MissionsController.cs
@@ -10,21 +10,33 @@ namespace Api.Application.Controllers;
 [Route("Api/[controller]")]
 public class MissionsController : ControllerBase
 {
+    //declaração de variável de IMissionsService 
     private readonly IMissionsService _service;
 
+    //inicialização da variável no construtor
     public MissionsController(IMissionsService service)
     {
         _service = service;
     }
 
+    //Todos os métodos da Controller retornam uma ActionResult que
+    //encapsula uma ResponseEntity contendo um campo bool Success
+    //e um object? Response
+
+    //método POST para cadastrar uma missão.
+    //Recebe um objeto da MissionsCreateDto e uma lista de strings com os nomes das 
+    //espaçonaves relacionadas à essa missão   
     [HttpPost]
     [Route("Create")]
     public async Task<ActionResult<ResponseEntity>> CreateMission([FromQuery] List<string> starshipNames, MissionsCreateDto mission)
     {
         try
         {
-            if (starshipNames.Count > 0)
+            //verifica se a lista contém algum nome e se a missão não é nula
+            //antes de fazer comunicação com a camada de serviço
+            if (starshipNames.Count > 0 && mission != null)
             {
+                //chama método do Service e aloca resultado na var response
                 var response = await _service.CreateMission(starshipNames, mission);
                 return Ok(response);
             }
@@ -38,14 +50,21 @@ public class MissionsController : ControllerBase
         }
     }
 
+    //método GET para obter as missões relacionadas ao 
+    //nome da espaçonave desejada pelo usuário.
+    //Esse método também retorna as informações da espaçonave em destaque.
+    //Recebe o nome da espaçonave como parâmetro  
     [HttpGet]
     [Route("GetByStarship/{starshipName}")]
     public async Task<ActionResult<ResponseEntity>> GetMissionsByStarship(string starshipName)
     {
         try
         {
+            //verifica se o nome é nulo antes de comunicar-se com
+            //a camada de serviço
             if(starshipName != null)
             {
+                //chama método do Service e aloca o resultado em uma var response 
                 var response = await _service.GetMissionsByStarship(starshipName);
                 return Ok(response);
             }
@@ -60,12 +79,15 @@ public class MissionsController : ControllerBase
         }
     }
 
+    //Método GET para obter todas as missões relacionadas à 
+    //todas as espaçonaves cadastradas no sistema
     [HttpGet]
     [Route("GetAll")]
     public async Task<ActionResult<ResponseEntity>> GetMissions()
     {
         try
         {
+            //chama método do Service e aloca o resultado em uma var response
             var response = await _service.GetMissions();
             return Ok(response);
         }
--- src/Api.Application/Controllers/StarshipController.cs
+++ src/Api.Application/Controllers/StarshipController.cs
@@ -10,13 +10,22 @@ namespace Api.Application.Controllers;
 [Route("Api/[controller]")]
 public class StarshipController : ControllerBase
 {
+    //declaração de variável de IStarshipService
     private readonly IStarshipService _service;
 
+    //inicialização da variável no construtor
     public StarshipController(IStarshipService service)
     {
         _service = service;
     }
 
+    //Todos os métodos da Controller retornam uma ActionResult que
+    //encapsula uma ResponseEntity contendo um campo bool Success
+    //e um object? Response
+
+    //método POST para cadastrar uma espaçonave.
+    //Recebe uma lista de strings com os modelos das 
+    //espaçonaves que serão absorvidas da Api SwapiDev
     [HttpPost]
     [Route("Create")]
     public async Task<ActionResult<ResponseEntity>> CreateStarship([FromQuery] List<string> models)
@@ -25,6 +34,8 @@ public class StarshipController : ControllerBase
         {
             if(models.Count > 0)
             {
+                //verifica se a lista contém algum modelo
+                //antes de fazer comunicação com a camada de serviço
                 var response = await _service.CreateStarship(models);
                 return Ok(response);
             }
@@ -39,14 +50,20 @@ public class StarshipController : ControllerBase
     }
 
 
+    //método GET para obter a espaçonave relacionada ao 
+    //nome da espaçonave desejada pelo usuário.
+    //Recebe o nome da espaçonave como parâmetro  
     [HttpGet]
     [Route("GetByName/{name}")]
     public async Task<ActionResult<ResponseEntity>> GetStarshipByName(string name)
     {
         try
         {
+            //verifica se o nome é nulo antes de comunicar-se com
+            //a camada de serviço
             if(name != null)
             {
+                //chama método do Service e aloca o resultado em uma var response
                 var response = await _service.GetStarshipByName(name);
                 return Ok(response);
             }
@@ -60,14 +77,21 @@ public class StarshipController : ControllerBase
         }
     }
 
+
+    //método GET para obter a espaçonave relacionada ao 
+    //modelo da espaçonave desejada pelo usuário.
+    //Recebe o modelo da espaçonave como parâmetro
     [HttpGet]
     [Route("GetByModel/{model}")]
     public async Task<ActionResult<ResponseEntity>> GetStarshipByModel(string model)
     {
         try
         {
+            //verifica se o modelo é nulo antes de comunicar-se com
+            //a camada de serviço
            if(model != null)
             {
+                //chama método do Service e aloca o resultado em uma var response
                 var response = await _service.GetStarshipByModel(model);
                 return Ok(response);
             }
@@ -81,14 +105,21 @@ public class StarshipController : ControllerBase
         }
     }
 
+
+    //método GET para obter a espaçonave relacionada ao 
+    //fabricante da espaçonave desejada pelo usuário.
+    //Recebe o fabricante da espaçonave como parâmetro
     [HttpGet]
     [Route("GetByManufacturer/{manufacturer}")]
     public async Task<ActionResult<ResponseEntity>> GetStarshipByManufacturer(string manufacturer)
     {
         try
         {
+            //verifica se o fabricante é nulo antes de comunicar-se com
+            //a camada de serviço
             if(manufacturer != null)
             {
+                //chama método do Service e aloca o resultado em uma var response
                 var response = await _service.GetStarshipByManufacturer(manufacturer);
                 return Ok(response);
             }
@@ -102,14 +133,21 @@ public class StarshipController : ControllerBase
         }
     }
 
+
+    //método PUT para atualizar a espaçonave desejada pelo usuário
+    //Recebe o nome da espaçonave e um objeto de StarshipUpdateDto
+    //como parâmetro
     [HttpPut]
     [Route("Update/{name}")]
     public async Task<ActionResult<ResponseEntity>> UpdateStarship(string name, StarshipUpdateDto starship)
     {
         try
         {
+            //verifica se o nome é nulo antes de comunicar-se com
+            //a camada de serviço
             if(name != null)
             {
+                //chama método do Service e aloca o resultado em uma var response
                 var response = await _service.UpdateStarship(name, starship);
                 return Ok(response);
             }
@@ -123,14 +161,20 @@ public class StarshipController : ControllerBase
         }
     }
 
+
+    //método DELETE para excluir a espaçonave desejada pelo usuário
+    //Recebe o nome da espaçonave como parâmetro
     [HttpDelete]
     [Route("Delete/{name}")]
     public async Task<ActionResult<ResponseEntity>> DeleteStarship(string name)
     {
         try
         {
+            //verifica se o nome é nulo antes de comunicar-se com
+            //a camada de serviço
             if(name != null)
             {
+                //chama método do Service e aloca o resultado em uma var response
                 var response = await _service.DeleteStarship(name);
                 return Ok(response);
             }
